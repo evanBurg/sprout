68483e53ee8e57a56912f38cbd2a87fff2290d8c
Evan Burgess <burgenator@hotmail.com>
2019-05-13 19:01:00 -0400

    Start with a base page and display plants in a list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Page,
+  Tabbar,
+  Tab,
+  Navigator
+} from 'react-onsenui';
+
+
+import Home from './Pages/Home/Home';
+
+class Tabs extends React.Component {
+  renderTabs = () => {
+    return [
+      {
+        content: <Home key="home" navigator={this.props.navigator} />,
+        tab: <Tab key="home" label="Home" icon="ion-ios-home-outline" />
+      },
+    ];
+  }
+
+  render() {
+    return (
+      <Page>
+        <Tabbar
+          renderTabs={this.renderTabs}
+        />
+      </Page>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  renderPage(route, navigator) {
+    route.props = route.props || {};
+    route.props.navigator = navigator;
+
+    return React.createElement(route.comp, route.props);
+  }
+
+  render() {
+    return (
+      <Navigator
+        initialRoute={{comp: Tabs, props: { key: 'tabs' }}}
+        renderPage={this.renderPage}
+      />
+    );
+  }
 }
 
 export default App;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,69 @@
+import React from "react";
+import ferns from "./ferns.json";
+import { Page, Toolbar, List, ListItem, ListHeader } from "react-onsenui";
+import PlantCard from "./PlantCard"
+import {capitalize} from '../../util'
+
+class Home extends React.Component {
+  state = {
+    plants: []
+  };
+
+  renderToolbar = (title) => {
+    return (
+      <Toolbar>
+        <div className="center">{title}</div>
+      </Toolbar>
+    );
+  }
+
+  gotoPlant = (component, key, plant) => {
+    this.props.navigator.pushPage({comp: component, props: { key, plant, navigator: this.props.navigator }});
+  }
+
+  getData = async () => {
+    // //Get client side token
+    // let JWT = await fetch("https://trefle.io/api/auth/claim?token=R0dGTXdVcng3Nk9DRk5DdlRrWWNNdz09&origin=localhost");
+    // JWT = await JWT.json();
+
+    // //Get ferns
+    // let res = await fetch(
+    //   `https://trefle.io/api/plants?q=fern&token=${JWT.token}`
+    // );
+    // if (res.ok) {
+    //     let plants = await res.json();
+    //   this.setState({
+    //     plants
+    //   });
+    // }
+    this.setState({
+      plants: ferns
+    });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <Page renderToolbar={() => this.renderToolbar('Home')}>
+        <List
+          renderHeader={() => <ListHeader>Ferns</ListHeader>}
+          dataSource={this.state.plants}
+          renderRow={plant => {
+            if (plant.common_name)
+              return (
+                <ListItem key={plant.id} tappable onClick={() => this.gotoPlant(PlantCard, `page-${plant.id}`, plant)}>
+                  {capitalize(plant.common_name)}
+                </ListItem>
+              );
+            else return <React.Fragment key={plant.id} />;
+          }}
+        />
+      </Page>
+    );
+  }
+}
+
+export default Home;
