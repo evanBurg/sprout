510087142a7ef73149e3c4baa197b705b6966ec0
Evan Burgess <burgenator@hotmail.com>
2019-07-07 18:02:08 -0400

    Add toast support

---
+++ Front End/src/Notifications/ToastContainer.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Context } from "../App";
+import { Toast } from 'react-onsenui'
+
+/*
+    Toast Structure
+    {
+        msg: String
+        animation: Enum (default | ascend | lift | fall | fade | none)
+        animationOptions: Object,
+        timeout: Number
+        Sound: URI
+    }
+*/
+
+class ToastContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      styles: {
+        container: {
+          position: "fixed",
+          bottom: 25,
+          left: 5,
+          right: 5
+        }
+      }
+    };
+  }
+
+  render() {
+    let { styles } = this.state;
+    let { toasts } = this.props;
+
+    toasts = []
+    return (
+      <Context.Consumer>
+        {context => <div style={styles.container}>
+            {toasts.map(toast => {
+                return <Toast animation={toast.animation} animationOptions={toast.animationOptions} timeout={toast.timeout}>
+                    {toast.msg}
+                </Toast>
+            })}
+        </div>}
+      </Context.Consumer>
+    );
+  }
+}
+
+export default ToastContainer;
